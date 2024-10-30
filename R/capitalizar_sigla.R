8e91b4b610a69e88b8ae6be428872e2e17d0f95f
Luis Alberto Ortega Ramirez <luisortegar99@gmail.com>
2024-10-29 21:01:10 -0600

    story 7: esto agrega la misma funcion pero con otro nombre ya que al descargarlo no subio la funcion

---
+++ R/capitalizar_sigla.R
@@ -0,0 +1,32 @@
+#' Capitalize Acronyms and Title Case Text
+#'
+#' This function capitalizes acronyms (uppercase sequences) and formats the remaining
+#' text in title case. It can be useful for text formatting where acronyms need to
+#' remain in uppercase while other parts of the text are capitalized.
+#'
+#' @param text A character string where the initial uppercase acronym will remain
+#'             capitalized, and the rest of the text will be converted to title case.
+#' @return A character string with the acronym in uppercase and the remaining text
+#'         in title case.
+#' @examples
+#' capitalizar_sigla('ESIA ZACATENCO')
+#' capitalizar_sigla('CICS MILPA ALTA')
+#' @export
+capitalizar_sigla <- function(text) {
+  # Separate the acronym (uppercase sequence) from the rest of the text
+  acronym <- gsub("\\s.*", "", text, perl = TRUE)
+  remaining_text <- gsub("^[^\\s]*\\s", "", text, perl = TRUE)
+
+  # Check if text is only the acronym
+  if (acronym == remaining_text) {
+    return(acronym)
+  } else {
+    # Capitalize the remaining text in title case
+    remaining_text <- str_to_title(tolower(remaining_text))
+
+    # Combine the uppercase acronym with the title-cased remaining text
+    result <- paste(acronym, remaining_text, sep = " ")
+
+    return(result)
+  }
+}
